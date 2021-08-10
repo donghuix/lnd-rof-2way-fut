76634e0f06578f720fbea4a140af0586d20dd402
Donghui Xu <donghui.xu@pnnl.gov>
2021-08-09 20:14:15 -0700

    read daily data for one year

--- Step_03_Reading_E3SM_output_one_year.m
+++ Step_03_Reading_E3SM_output_one_year.m
@@ -7,11 +7,10 @@ scenarios = {'GFDL_FUT_1way_LLR3_7e85a10.2021-07-13-145042', ...
              'GFDL_FUT_noinund_LLR3_7e85a10.2021-07-14-194034'};
 tag = {'GFDL_FUT_1way','GFDL_FUT_2way','GFDL_FUT_noinund'};
 
-for i = 3%1 : length(scenarios)
-    qmax = NaN(720,360,86);
-    tmax = NaN(720,360,86);
-    fmax = NaN(720,360,86);
-    for iyr = 1 : 86
+for i = 1 : 2%length(scenarios)
+    qd = NaN(720,360,365);
+    fd = NaN(720,360,365);
+    for iyr = 86
         if iyr < 10 
             files = dir([wkdir scenarios{i} '/run/*mosart.h1.000' num2str(iyr) '*.nc']);
         elseif iyr < 100
@@ -20,15 +19,15 @@ for i = 3%1 : length(scenarios)
             files = dir([wkdir scenarios{i} '/run/*mosart.h1.0' num2str(iyr) '*.nc']);
         end
         q2 = NaN(720,360,length(files),2);
-        data = cat_mosart(files,{'RIVER_DISCHARGE_OVER_LAND_LIQ','RIVER_DISCHARGE_TO_OCEAN_LIQ'}); % ,'FLOODPLAIN_FRACTION'
+        data = cat_mosart(files,{'RIVER_DISCHARGE_OVER_LAND_LIQ','RIVER_DISCHARGE_TO_OCEAN_LIQ','FLOODPLAIN_FRACTION'}); % ,'FLOODPLAIN_FRACTION'
         q2(:,:,:,1) = data.RIVER_DISCHARGE_OVER_LAND_LIQ;
         q2(:,:,:,2) = data.RIVER_DISCHARGE_TO_OCEAN_LIQ;
-        q2 = nanmean(q2,4);
-        [qmax(:,:,iyr),tmax(:,:,iyr)] = max(q2,[],3);
-        %fmax(:,:,iyr) = max(data.FLOODPLAIN_FRACTION,[],3);
+        qd = nanmean(q2,4);
+        %[qmax(:,:,iyr),tmax(:,:,iyr)] = max(q2,[],3);
+        fd = data.FLOODPLAIN_FRACTION;
     end
     if ~exist('data','dir')
         mkdir('data');
     end
-    save(['data/' tag{i} '_dmax.mat'],'qmax','tmax','fmax');
+    save(['data/' tag{i} '_' num2str(iyr) '.mat'],'qd','fd');
 end
